c7e4a112ad185609b92e01a0386546bc5d61705b
1Mirl <miredi.lunga@colombbus.org>
2021-11-26 16:52:33 +0100

    correction Imran
    correction

---
+++ imranLOCATION/js/locationV.js
@@ -0,0 +1,6 @@
+// alert('hello')
+
+function myFunction() {
+    document.getElementById("formInscription").innerHTML = '<p style="background:lightgreen; color:#fff; text-align: center; padding: 5px;">Your order has been submited</p>';
+  //   alert('Your order has been submited');
+  }
